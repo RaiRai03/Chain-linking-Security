8d9281a6ec19a45e9c6e66a9cc9100eb4fdfe6ae
RaiRai03 <139826907+RaiRai03@users.noreply.github.com>
2024-03-12 15:40:36 -0400

    Python code that simulates a blockchain-based supply chain tracking system

---
+++ Example1.py
@@ -0,0 +1,30 @@
+import hashlib
+import time
+
+class Block:
+    def __init__(self, index, previous_hash, timestamp, data, hash):
+        self.index = index
+        self.previous_hash = previous_hash
+        self.timestamp = timestamp
+        self.data = data
+        self.hash = hash
+
+def calculate_hash(index, previous_hash, timestamp, data):
+    value = str(index) + str(previous_hash) + str(timestamp) + str(data)
+    return hashlib.sha256(value.encode('utf-8')).hexdigest()
+
+def create_genesis_block():
+    return Block(0, "0", int(time.time()), "Genesis Block", calculate_hash(0, "0", int(time.time()), "Genesis Block"))
+
+def create_new_block(previous_block, data):
+    index = previous_block.index + 1
+    timestamp = int(time.time())
+    hash = calculate_hash(index, previous_block.previous_hash, timestamp, data)
+    return Block(index, previous_block.previous_hash, timestamp, data, hash)
+
+# Create the blockchain and add the genesis block
+blockchain = [create_genesis_block()]
+previous_block = blockchain[0]
+
+# How many blocks should we add to the chain
+#
